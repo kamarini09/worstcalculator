fd9bcd096311852034e6eee66d358da4586d2f5c
kamarini <kama0549@stud.kea.dk>
2023-02-03 18:42:17 +0100

    read from dropdown and all operations are working

--- script.js
+++ script.js
@@ -3,16 +3,29 @@
 function getInputValue(){
     document.querySelector('input#firstnumber').addEventListener('input', updateValue);
     document.querySelector('input#secondnumber').addEventListener('input', updateValue);
-    document.querySelector('button#calculate').addEventListener('click', addNumbers);
+    document.querySelector('button#calculate').addEventListener('click', makeOperation);
   
 }
 
-function addNumbers() {
+function makeOperation() {
     const firstNumber = document.querySelector('input#firstnumber').value;
     const secondNumber = document.querySelector('input#secondnumber').value;
-    document.querySelector('input#firstnumber').value = Number(firstNumber) + Number(secondNumber);
-    document.querySelector('input#secondnumber').value = " ";
+    const chooseOperator = document.getElementById("operator");
+     const value = chooseOperator.value;
+     console.log(value);
+     if (value === "add"){
+        document.querySelector("input#firstnumber").value =  Number(firstNumber) + Number(secondNumber);
+    }
+    else if (value === "sub"){
+        document.querySelector("input#firstnumber").value =  Number(firstNumber) - Number(secondNumber);
+    }
+    else if (value === "mul"){
+        document.querySelector("input#firstnumber").value =  Number(firstNumber) * Number(secondNumber);
+    }
+    else { document.querySelector("input#firstnumber").value =  Number(firstNumber) / Number(secondNumber);}
+    document.querySelector('input#secondnumber').value = "";
 
+    
 }
 getInputValue();
 
