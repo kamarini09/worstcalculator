47c019e1a740f7be353a5c7dd7d800c8015c4365
kamarini <kama0549@stud.kea.dk>
2023-02-04 20:57:02 +0100

    tide up the code

--- script.js
+++ script.js
@@ -8,31 +8,68 @@ function getInputValue(){
 }
 
 function makeOperation() {
-    const firstNumber = document.querySelector('input#firstnumber').value;
-    const secondNumber = document.querySelector('input#secondnumber').value;
+    const firstNumber = Number(document.querySelector('input#firstnumber').value);
+    const secondNumber = Number(document.querySelector('input#secondnumber').value);
     // read operator from a drop down
     const chosenOperator = document.getElementById("operator");
     const value = chosenOperator.value;
     const newListNumber = document.createElement("li");
     console.log(value);
+    let result = "unused value";
     // regarding the append: there is probably a better way to do that. it's too long and redundant
     if (value === "add"){
-        document.querySelector("input#firstnumber").value =  Number(firstNumber) + Number(secondNumber);
-        document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) + Number(secondNumber);
+        result = firstNumber + secondNumber; 
     }
     else if (value === "sub"){
-        document.querySelector("input#firstnumber").value =  Number(firstNumber) - Number(secondNumber);
-        document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) - Number(secondNumber);
+        result = firstNumber - secondNumber;
     }
     else if (value === "mul"){
-        document.querySelector("input#firstnumber").value =  Number(firstNumber) * Number(secondNumber);
-        document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) * Number(secondNumber);
-    } else { document.querySelector("input#firstnumber").value =  Number(firstNumber) / Number(secondNumber);
-    document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) / Number(secondNumber);}
-    document.querySelector('input#secondnumber').value = "";
+        result = firstNumber * secondNumber;  
+    } else { 
+        result = firstNumber / secondNumber;
+    }
+    
+    document.querySelector("input#firstnumber").value =  result;
+    document.querySelector("ul#results").appendChild(newListNumber).textContent = result;
+    document.querySelector('input#secondnumber').value = ""; 
+}
 
     
-}
+// old one
+// function makeOperation() {
+//     const firstNumber =document.querySelector('input#firstnumber').value;
+//     const secondNumber = document.querySelector('input#secondnumber').value;
+//     // read operator from a drop down
+//     const chosenOperator = document.getElementById("operator");
+//     const value = chosenOperator.value;
+//     const newListNumber = document.createElement("li");
+//     console.log(value);
+    
+//     // regarding the append: there is probably a better way to do that. it's too long and redundant
+//     if (value === "add"){
+        
+//         // document.querySelector("input#firstnumber").value =  Number(firstNumber) + Number(secondNumber);
+//         // document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) + Number(secondNumber);
+//     }
+//     else if (value === "sub"){
+        
+//         // document.querySelector("input#firstnumber").value =  Number(firstNumber) - Number(secondNumber);
+//         // document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) - Number(secondNumber);
+//     }
+//     else if (value === "mul"){
+        
+//         // document.querySelector("input#firstnumber").value =  Number(firstNumber) * Number(secondNumber);
+//         // document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) * Number(secondNumber);
+//     } else { 
+      
+//     //     document.querySelector("input#firstnumber").value =  Number(firstNumber) / Number(secondNumber);
+//     // document.querySelector("ul#results").appendChild(newListNumber).textContent =  Number(firstNumber) / Number(secondNumber);}
+
+   
+//     document.querySelector('input#secondnumber').value = "";
+
+    
+// }
 getInputValue();
 
 
